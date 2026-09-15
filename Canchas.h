6f24ae4fd93f00d43b10d5acd5c887e8b80ba02d
dcs35 <daniela.carvajal.sanchez@est.una.ac.cr>
2026-09-15 00:56:02 -0600

    Add files via upload

---
+++ Canchas.h
@@ -0,0 +1,37 @@
+#ifndef CANCHA_H
+#define CANCHA_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int CANCHAS = 10;  
+
+class Cancha {
+private:
+    string codigo;
+    string deporte;
+    double precio;
+    char v[12];  // Guarda: L, O, M
+
+public:
+    Cancha();
+    Cancha(string cod, string dep, double pre);
+
+    string getCodigo();
+    string getDeporte();
+    double getPrecio();
+    void setPrecio(double nuevoPrecio);
+
+    void mostrarCancha();
+    void mostrarDis();  // las 12
+
+    bool libre(int horaInicial, int horaFinal);
+    void ocupado(int horaInicial, int horaFinal);   // marca como O
+    void liberar(int horaInicial, int horaFinal);   // marca como L
+    void mantenimiento(int horaInicial, int horaFinal); // marca como M
+    void quitarmant(int horaInicial, int horaFinal);    // vuelve a L
+};
+
+#endif
