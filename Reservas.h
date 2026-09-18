7d76da0332e2eb07a4940cc26d793016f87ec073
dcs35 <daniela.carvajal.sanchez@est.una.ac.cr>
2026-09-17 23:13:34 -0600

    Refactor Reserva class and update header guards

--- Reservas.h
+++ Reservas.h
@@ -1,15 +1,30 @@
-#ifdef RESERVAS_H
+#ifndef RESERVAS_H
 #define RESERVAS_H
 #include <iostream>
-class Reserva{
-    const int reser=120;
-    private:
-    int consecutivo;
-    Cliente *cliente;
-    Cancha *cancha;
-    string deporte;
-    int monto;
-    string estado;
+#include <string>
+#include "Cliente.h"
+#include "Canchas.h"
+using namespace std;
+class Reserva {
+private:
+    int numReser;
+    Cliente* cliente;
+    Cancha* cancha;
     int horaInicial;
-    int horaFinal;
-}
+    int cant;
+    double monto;
+    string estado;
+    static int consecutivo;
+public:
+    Reserva(Cliente* cli, Cancha* canch, int ini, int can);
+    int getnumReser();
+    Cliente* getCliente();
+    Cancha* getCancha();
+    int getInicial();
+    int getCant();
+    double getMonto();
+    string getEstado();
+    void mostrarReserva();
+    void cancelarReserva();
+};
+#endif
